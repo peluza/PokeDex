7dec795a258c336eed08a4518efb017e0c5681b8
edison <edisonisaza@gmail.com>
2023-04-24 18:25:39 -0500

    Consultando la API de detalles del Pokemon

--- pokedex/src/screens/Pokemon.js
+++ pokedex/src/screens/Pokemon.js
@@ -1,10 +1,29 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getPokemonDetailsApi } from '../api/pokemon'
 
-export default function Pokemon() {
+export default function Pokemon(props) {
+  const { navigation, route: { params } } = props
+  console.log(params.id)
+  const [ pokemon, setPokemon] = useState(null)
+
+  useEffect(()=> {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi(params.id)
+        setPokemon(response)
+      } catch (error) {
+        navigation.goBack()
+      }
+    })()
+    
+  }, [params])
+
+  if (!pokemon) return null
   return (
     <View>
       <Text>Pokemon</Text>
+      <Text>{pokemon.name}</Text>
     </View>
   )
 }
