640005ec60e0129438b4dee068eec8641e9d9740
edison <edisonisaza@gmail.com>
2023-04-24 18:10:36 -0500

    Vista y navegación al detalle del Pokemon

--- pokedex/src/components/PokemonCard.js
+++ pokedex/src/components/PokemonCard.js
@@ -2,13 +2,15 @@ import { StyleSheet, View, Text, Image, TouchableWithoutFeedback } from 'react-n
 import React from 'react'
 import getColorByPokemonType from '../utils/getColorByPokemonType'
 import { capitalize } from "lodash"
+import { useNavigation } from "@react-navigation/native";
 
 export default function PokemonCard(props) {
     const { pokemon } = props
+    const navigation = useNavigation()
     const pokemonColor = getColorByPokemonType(pokemon.type)
     const bgStyles = { backgroundColor: pokemonColor, ...styles.bgStyles }
     const goToPokemon = () => {
-        console.log(`vamos en ${pokemon.name}`)
+        navigation.navigate("Pokemon", { id: pokemon.id})
     }
 
     return (
