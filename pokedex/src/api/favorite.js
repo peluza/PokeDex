ef462fb1c330ad6941ae2d45af2dab17c7702bf5
edison <edisonisaza@gmail.com>
2023-05-01 14:24:11 -0500

    Almacenando varios Pokémon favoritos

--- pokedex/src/api/favorite.js
+++ pokedex/src/api/favorite.js
@@ -5,8 +5,8 @@ import { FAVORITE_STORAGE } from "../utils/constants"
 
 export async function getPokemonsFavoriteApi() {
     try {
-        const respose = AsyncStorage.getItem(FAVORITE_STORAGE)
-        return respose
+        const respose = await AsyncStorage.getItem(FAVORITE_STORAGE)
+        return JSON.parse(respose || '[]')
     } catch (error) {
         throw error
     }
@@ -14,8 +14,8 @@ export async function getPokemonsFavoriteApi() {
 
 export async function addPokemonFavoriteApi(id) {
     try {
-        const favorites = []
-        favorites.push
+        const favorites = await getPokemonsFavoriteApi()
+        favorites.push(id)
         await AsyncStorage.setItem(FAVORITE_STORAGE, JSON.stringify(favorites))
         console.log()
     } catch (error) {
