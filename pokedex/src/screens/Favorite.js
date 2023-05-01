de44b07071a89c274d6594facae160a010354032
edison <edisonisaza@gmail.com>
2023-05-01 17:49:46 -0500

    Lista de Pokémon favoritos

--- pokedex/src/screens/Favorite.js
+++ pokedex/src/screens/Favorite.js
@@ -1,18 +1,47 @@
-import { Button, SafeAreaView, Text } from 'react-native'
-import React from 'react'
+import { Text } from 'react-native'
+import React, { useState,  useCallback } from 'react'
+import { useFocusEffect } from '@react-navigation/native'
 import { getPokemonsFavoriteApi } from '../api/favorite'
+import useAuth from '../hooks/useAuth'
+import { getPokemonDetailsApi } from '../api/pokemon'
+import PokemonList from '../components/PokemonList'
+
+
 
 export default function Favorite() {
 
-  const checkFavorite = async () => {
-    const respose = await getPokemonsFavoriteApi()
-    console.log(respose)
+  const [pokemons, setPokemons] = useState([])
+  const { auth } = useAuth()
 
-  }
+  useFocusEffect(useCallback(() => {
+      if (auth) {
+        (async () => {
+          const response = await getPokemonsFavoriteApi()
+          const pokemonsArray = []
+  
+          for await (const pokemon of response) {
+            const pokemonDetails = await getPokemonDetailsApi(pokemon);
+  
+            pokemonsArray.push({
+              id: pokemonDetails.id,
+              name: pokemonDetails.name,
+              type: pokemonDetails.types[0].type.name,
+              order: pokemonDetails.order,
+              image: pokemonDetails.sprites.other["official-artwork"].front_default,
+            })
+          }
+          setPokemons(pokemonsArray)
+        })()
+      }
+  
+    }, [auth]))
+
+
   return (
-    <SafeAreaView>
-      <Text>Favorite</Text>
-      <Button title='pokemon' onPress={checkFavorite} />
-    </SafeAreaView>
+    !auth ? (
+      <Text>Usurio no logueado</Text>
+    ) : (
+      <PokemonList pokemons={pokemons} />
+    )
   )
 }
